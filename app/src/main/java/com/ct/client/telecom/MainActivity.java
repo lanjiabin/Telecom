1125cf6d83076c083265a963f6247873f0d9d659
lanjiabin <1323457707@qq.com>
2020-06-13 13:46:53 +0800

    中国电信营业厅

---
+++ app/src/main/java/com/ct/client/telecom/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ct.client.telecom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
